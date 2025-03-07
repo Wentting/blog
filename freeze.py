3ce7e80278f8bccd04231e87742798781539fd3a
Wentting <767000900@qq.com>
2025-03-07 14:53:29 +0800

    test

--- freeze.py
+++ freeze.py
@@ -2,8 +2,6 @@ from flask_frozen import Freezer
 from app import app
 import os
 
-# 设置基础URL为仓库名称
-app.config['FREEZER_BASE_URL'] = 'https://wentting.github.io/blog/'
 # 设置输出目录为 docs
 app.config['FREEZER_DESTINATION'] = 'docs'
 # 确保静态文件路径正确
@@ -17,11 +15,21 @@ freezer = Freezer(app)
 #     for filename in os.listdir(notes_dir):
 #         if filename.endswith('.md'):
 #             yield {'note_id': filename.replace('.md', '')}
+
+
 @freezer.register_generator
-def index():
+def home():
     # 确保生成首页
+    yield {}
+
+@freezer.register_generator
+def publications():
+    yield {}
+
+@freezer.register_generator
+def projects():
     yield {}
     
 if __name__ == '__main__':
     freezer.freeze()
-    print("静态网站生成完成，请确保index.html文件在根目录下")
+    print("静态网站已生成到 docs 目录")
